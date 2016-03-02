79f84ddaecad510e8a0e5168d242c0656c360a4a
DevBill <joe_wang113@163.com>
2016-03-02 15:32:55 +0800

    Initial Commit

---
+++ GIT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GIT
+//
+//  Created by 王备 on 16/3/2.
+//  Copyright © 2016年 wangbei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GIT/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GIT
+//
+//  Created by 王备 on 16/3/2.
+//  Copyright © 2016年 wangbei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
